136165a3cc9d832cdfa72e168eb1f1d464c132a5
Андрей <nevraev1109@mail.ru>
2022-12-27 21:58:59 +0500

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        { 
+            InitializeComponent();
+           
+        }
+
+        private void Button(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Image files| *.jpg;*.png";
+            openDialog.FilterIndex = 1;
+            if (openDialog.ShowDialog()==true)
+            {
+                imagePicture.Source = new BitmapImage(new Uri(openDialog.FileName));
+            }
+        }
+    }
+}
